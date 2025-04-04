fea1b7b37eb73348f47372463e80affe2b05a591
mrMcAko <alechacin08@gmail.com>
2025-04-03 20:34:27 -0600

    Corrección de codigo

--- knexfile.js
+++ knexfile.js
@@ -10,11 +10,11 @@ module.exports = {
     connection: {
       host:  process.env.DB_HOST_DEV, 
       database: process.env.DB_NAME_DEV,
-      user:     process.env.DB_USER_DEV,
+      user: process.env.DB_USER_DEV,
       password: process.env.DB_PASSWORD_DEV
     },
     pool: {
-      min: 2,
+      min: 2,     
       max: 10
     },
     migrations: {
