82554beec0cdaedd44549d0a22bc7dd4dd8238fd
mrMcAko <alechacin08@gmail.com>
2025-04-11 23:26:07 -0600

    Agregamos "homeRoutes" a las rutas del index

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const homeRoutes = require('./routes/homeRoutes')
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use('/api', homeRoutes) // Definir la ruta base para las rutas de la API
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
