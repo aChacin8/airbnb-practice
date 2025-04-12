0e38155a1383d847a3093d88effe43f13d4b9f9a
mrMcAko <alechacin08@gmail.com>
2025-04-11 22:42:14 -0600

    Agregamos la configuracion del switch para el archivo knexfile.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const environment = process.env.NODE_ENV || 'development' // 'development' es el entorno por defecto, si no se especifica ninguna variable de entorno para la ejecución
+const knex = require ('knex')
+const knexFile = require ('./knexfile.js')
+
+//Mandar a llamar la configuracion de knexfile.js adecuada
+module.exports = knex (knexfile[environment]) // knexfile[development] o knexfile[staging] o knexfile[production], son las variables de entorno en el que se ejecutará el servidor
