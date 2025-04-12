d064c121fa5901f5cdd2f104ca9c51e6b6d9c678
mrMcAko <alechacin08@gmail.com>
2025-04-12 00:23:20 -0600

    Agregamos la ruta de verbo GET para filtrar por id

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -5,5 +5,6 @@ const homeController = require('../controllers/homeController') // Importar el c
 
 router.post('/homes', homeController.createHome) // Definir la ruta para crear un nuevo hogar, llamando a la función createHome del controlador de Home
 router.get('/homes', homeController.findAllhomes) // Definir la ruta para obtener todos los hogares, llamando a la función findAllhomes del controlador de Home
+router.get('/homes/:idHome', homeController.findById) // Definir la ruta para obtener un hogar por su id, llamando a la función findById del controlador de Home
 
 module.exports = router // Exportar el router para que pueda ser utilizado en otros archivos
