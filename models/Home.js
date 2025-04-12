1ac6f7d19c9be3f12e15f259a713a40d584cc10d
mrMcAko <alechacin08@gmail.com>
2025-04-12 13:14:40 -0600

    Creamos el modelo de update, softDelete, y destroy

--- models/Home.js
+++ models/Home.js
@@ -23,8 +23,35 @@ const findById = (houseId) => {
     .where('active', true)
 }
 
+const update = (houseId, bodyHome) => {
+  return knex
+    .update(bodyHome) // bodyHome es el objeto que contiene los datos del nuevo hogar a crear
+    .from('homes')
+    .where('house_id', houseId) // Filtra los resultados para que solo devuelva el hogar con el id indicado
+    .where('active', true) // Filtra los resultados para que solo devuelva los hogares activos
+    .returning('*') //
+}
+
+// SofDelete No elimina el registro de la BD solo lo cambia a inactivo
+const softDelete = (houseId) => {
+  return knex
+    .update({ active: false })
+    .from('homes')
+    .where('house_id', houseId) // Filtra los resultados para que solo devuelva el hogar con el id indicado
+}
+
+// HardDelete Elimina el registro de la BD
+const destroy = (houseId) => {
+  return knex
+    .delete()
+    .from('homes')
+    .where('house_id', houseId) // Importante que tenga el where para no eliminar toda la tabla, solo el registro que queremos eliminar
+}
 module.exports = {
   create,
   findAll,
-  findById
+  findById,
+  update,
+  softDelete,
+  destroy
 }
