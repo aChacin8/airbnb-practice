98da1a93b7ffe99137cdcbf5478bcf54c574ecd0
mrMcAko <alechacin08@gmail.com>
2025-04-11 23:56:19 -0600

    Creamos la ruta  GET para visualizar todas las casas activas

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 const homeController = require('../controllers/homeController') // Importar el controlador de Home
+
 router.post('/homes', homeController.createHome) // Definir la ruta para crear un nuevo hogar, llamando a la función createHome del controlador de Home
+router.get('/homes', homeController.findAllhomes) // Definir la ruta para obtener todos los hogares, llamando a la función findAllhomes del controlador de Home
 
 module.exports = router // Exportar el router para que pueda ser utilizado en otros archivos
